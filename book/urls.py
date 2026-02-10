b05de08dcce15b8b56485c0a886c8c494fd7c66c
Elvira <ekadirova63@gmail.com>
2026-02-10 13:18:12 +0600

    crud

--- book/urls.py
+++ book/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('books/', views.book_list),
     path('books/<int:id>/', views.book_detail),
+    path('books/<int:id>/delete/', views.delete_book_view),
+    path('books/<int:id>/update/', views.update_book_view),
+    path('create_book/', views.create_book_view),
 ]
--- book/views.py
+++ book/views.py
@@ -1,6 +1,48 @@
-from django.shortcuts import render, get_object_or_404
-from . import models
+from django.shortcuts import render, get_object_or_404, redirect
+from . import models, forms
 
+#UPDATE BOOK
+def update_book_view(request, id):
+    book_id = get_object_or_404(models.Books, id=id)
+    if request.method == "POST":
+        form = forms.BookForm(request.POST, instance=book_id)
+        if form.is_valid():
+            form.save()
+            return redirect('/book/')
+    else:
+        form = forms.BookForm(instance=book_id)
+    return render(
+        request,
+        "update_book.html",
+        {
+            "form": form,
+            "book_id": book_id
+        }
+    )
+
+#DELETE BOOK
+def delete_book_view(request, id):
+    book_id = get_object_or_404(models.Books, id=id)
+    book_id.delete()
+    return redirect('/book/')
+
+#CREATE BOOK
+def create_book_view(request):
+    if request.method == "POST":
+        form = forms.BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/book/')
+    else:
+        form = forms.BookForm()
+        
+    return render(
+        request,
+        "create_book.html",
+        {
+            "form": form
+        }
+    )
 
 def book_detail(request, id):
     if request.method == 'GET':
